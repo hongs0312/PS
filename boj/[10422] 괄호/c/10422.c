b8245cf5b3103288e699c0aa9d45348c01733472
hongs0312 <73696369+hongs0312@users.noreply.github.com>
2024-03-07 17:32:34 +0900

    [boj] 괄호 (Time: 12ms, Memory: 1.16MB) - by Code-Vault

---
+++ boj/[10422] 괄호/c/10422.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#define MOD 1000000007
+
+long long int dp[5001]; //pd[i] = 길이가 i일 때 만들 수 있는 괄호 문자열의 개수
+int input[1000];
+
+int main(){
+    int t;
+    scanf("%d", &t);
+
+    int max = 0;
+    for(int i = 0; i < t; i++){
+        scanf("%d", &input[i]);
+
+        if(max < input[i]) max = input[i];
+    }
+    dp[0] = dp[2] = 1;
+    for(int i = 4; i <= max; i+=2)
+        for(int j = 0; j < i; j+=2)
+            dp[i] = (dp[i] + dp[j] * dp[i-j-2]) % MOD;
+        
+
+    for(int i = 0; i < t; i++)
+        printf("%lld\n", dp[input[i]]);
+    
+    return 0;
+}
---
+++ boj/[10422] 괄호/c/README.md
@@ -0,0 +1,8 @@
+# [10422] 괄호
+
+[문제 출처](https://www.acmicpc.net/problem/10422)
+
+## 성능
+
+- Time: 12ms
+- Memory: 1.16MB
