9049d9a38de8ea0c492070fe76ef0cadf06d0bf8
hongs0312 <73696369+hongs0312@users.noreply.github.com>
2024-03-07 17:48:26 +0900

    [boj] 괄호 (Time: 12ms, Memory: 1.16MB) - by Code-Vault

--- boj/[10422] 괄호/c/10422.c
+++ boj/[10422] 괄호/c/10422.c
@@ -1,7 +1,10 @@
+/*
+괄호
+*/
 #include <stdio.h>
 #define MOD 1000000007
 
-long long int dp[5001]; //pd[i] = 길이가 i일 때 만들 수 있는 괄호 문자열의 개수
+long long int dp[5001]; //dp[i] = 길이가 i일 때 만들 수 있는 괄호 문자열의 개수
 int input[1000];
 
 int main(){
@@ -16,8 +19,8 @@ int main(){
     }
     dp[0] = dp[2] = 1;
     for(int i = 4; i <= max; i+=2)
-        for(int j = 0; j < i; j+=2)
-            dp[i] = (dp[i] + dp[j] * dp[i-j-2]) % MOD;
+        for(int j = 2; j <= i; j+=2)
+            dp[i] = (dp[i] + dp[j-2] * dp[i-j]) % MOD;
         
 
     for(int i = 0; i < t; i++)
