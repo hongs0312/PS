f558fe3cfd9e55ec7ef4c3912a82af48f619ee57
hongs0312 <73696369+hongs0312@users.noreply.github.com>
2024-04-05 15:30:05 +0900

    [boj] 제곱 ㄴㄴ (Time: 20ms, Memory: 2.384MB) - by Code-Vault

---
+++ boj/[1557] 제곱 ㄴㄴ/cpp/1557.cpp
@@ -0,0 +1,71 @@
+#include <bits/stdc++.h>
+
+
+#define MAX 47000
+
+using namespace std;
+
+bool num[MAX];
+vector<int> prime;
+
+int sqCnt[MAX];
+
+void getPrime();
+long long getCnt(int idx, int num, int x, bool include);
+int main(){
+    ios::sync_with_stdio(false);
+
+    int k;
+    cin >> k;
+
+    getPrime();
+
+    long long left = k, right = k*2 + 1, mid, cnt;
+    int n;
+    while(left < right) {
+        mid = (left + right) / 2;
+        
+        for(int i = 2; i*i <= mid; i++) //mid보다 작은 각 제곱수의 배수 개수
+            sqCnt[i] = mid/(i*i);
+
+        n = mid - getCnt(0, 1, mid, true); //n번째 제곱 ㄴㄴ수
+
+        if(n > k) right = mid - 1;
+        else if (n < k) left = mid + 1;
+        else right = mid;
+    }
+    cout << left << endl;
+
+    return 0;
+}
+void getPrime() {
+    for(int i = 2; i*i < MAX; i++) {
+        if(num[i] == false) {
+            for(int j = i+i; j < MAX; j += i)
+                num[j] = true;
+        }
+    }
+    for(int i = 2; i < MAX; i++) {
+        if(num[i] == false)
+            prime.push_back(i);
+    }
+}
+// idx : 소수 인덱스, num : 현재까지의 소수 곱, x : 입력값, include : 현재 소수를 포함인가 배제인가
+long long getCnt(int idx, int num, int x, bool include) {
+    long long result, primeComb, ret = 0;
+    int cnt;
+    for(int i = idx; i < prime.size(); i++) {
+        primeComb = num*prime[i];
+
+        if(primeComb*primeComb > x)
+            break;
+
+        cnt = sqCnt[primeComb];
+        if(include == false)
+            cnt *= -1;
+
+        ret += cnt;
+        ret += getCnt(i+1, primeComb, x, !include);
+    }
+    return ret;
+}
---
+++ boj/[1557] 제곱 ㄴㄴ/cpp/README.md
@@ -0,0 +1,8 @@
+# [1557] 제곱 ㄴㄴ
+
+[문제 출처](https://www.acmicpc.net/problem/1557)
+
+## 성능
+
+- Time: 20ms
+- Memory: 2.384MB
